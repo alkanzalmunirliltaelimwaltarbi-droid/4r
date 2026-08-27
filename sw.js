06f531fdfebd7de7acb0d06a7a641dd316e3f605
alkanzalmunirliltaelimwaltarbi-droid <alkanzalmunirliltaelimwaltarbi@gmail.com>
2026-08-27 19:02:17 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE='marble-pwa-final-v23';
+const CORE=['./','./index.html','./manifest.json','./icon-192.svg','./icon-512.svg'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  if(event.request.mode==='navigate'){
+    event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put('./index.html',copy));return response}).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match('./index.html'))));
+});
